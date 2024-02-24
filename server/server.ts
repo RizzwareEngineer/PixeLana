f623b90afd230c8f2383eee412ae0341fd3fc368
BumbleIV <linganda@msu.edu>
2024-02-24 11:44:40 -0600

    Pushing to push

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,43 @@
-import { Server as SocketIOServer } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { createServer } from 'http';
+import { Player } from './models/player';
+import { Prompt } from './models/prompt';
 
 const httpServer = createServer();
-const io = new SocketIOServer(httpServer, {
+const io = new Server(httpServer, {
     cors: {
         origin: '*',
         methods: ['GET', 'POST']
     }
 });
 
-let numberPlayers: number = 0; 
+let players: Record<string, Player> = {};
+let startingPrompts: Prompt[] = [];
 
 // Player connects
-io.on('connection', (socket) => {
-    numberPlayers++;
-    console.log(`a user connected. Total players: ${numberPlayers}`);
+io.on('connect', (socket) => {
+    
+    // Add player to memory
+    socket.on('addPlayer', (name, publicKey) => {
+        let player = { id: socket.id, name, publicKey };
+        players[socket.id] = player;
+        
+        console.log(`User ${socket.id} connected. Total players: ${Object.keys(players).length}`);
+      });
+
     
     // Player disconnects
     socket.on('disconnect', () => {
-        numberPlayers--;
-        console.log('user disconnected. Total players: ' + numberPlayers);
+        let player = players[socket.id];
+        if (!player) {
+            console.log(`User ${socket.id} not found.`);
+            return;
+        }
+
+        delete players[socket.id];
+        console.log(`User ${socket.id} disconnected. Total players: ${Object.keys(players).length - 1}`);
     });
 });
+
 const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
